d81abeebc0d48f8e720584e37a6de5a1c1afca6b
Arian Rezazadeh <notarian.rez@gmail.com>
2023-03-28 17:54:56 +0330

    validate and hash password

--- src/auth/register.rs
+++ src/auth/register.rs
@@ -7,6 +7,7 @@ use actix_web::{
     web::{Data, Json},
 };
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 
 #[derive(Serialize, Deserialize)]
 pub struct RegisterArgs {
@@ -19,27 +20,53 @@ pub struct RegisterArgs {
 pub async fn register(args: Json<RegisterArgs>, pool: Data<DbPool>) -> ApiResult<&'static str> {
     validate_phone_number(&args.phone_number)?;
     validate_name(&args.name)?;
-    insert_user(&pool, &args.name, &args.password, &args.phone_number).await?;
+    validate_password(&args.password)?;
+
+    let hashed_password = sha256_hash(&args.password);
+    insert_user(&pool, &args.name, &hashed_password, &args.phone_number).await?;
     Ok("")
 }
 
 fn validate_phone_number(phone_number: &str) -> ApiResult<()> {
     let has_valid_length = phone_number.len() == 11;
     let has_valid_characters = phone_number.chars().all(|char| char.is_ascii_digit());
-
     if has_valid_length && has_valid_characters {
         Ok(())
     } else {
-        Err(ApiError::BadArgument { argument_name: "phone_number" })
+        Err(ApiError::BadArgument {
+            argument_name: "phone_number",
+        })
     }
 }
 
 fn validate_name(name: &str) -> ApiResult<()> {
-    if !name.chars().all(|char| char.is_ascii_alphanumeric()) {
-        Err(ApiError::BadArgument { argument_name: "name" })
-    } else {
+    let has_valid_length = name.len() > 2 || name.len() <= 16;
+    let has_valid_characters = name.chars().all(|char| char.is_ascii_alphanumeric());
+    if has_valid_characters && has_valid_length {
         Ok(())
+    } else {
+        Err(ApiError::BadArgument {
+            argument_name: "name",
+        })
     }
+}
+
+fn validate_password(password: &str) -> ApiResult<()> {
+    let has_valid_length = password.len() >= 5 || password.len() <= 64;
+    if has_valid_length {
+        Ok(())
+    } else {
+        Err(ApiError::BadArgument {
+            argument_name: "password",
+        })
+    }
+}
+
+fn sha256_hash(input: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    format!("{:x}", result)
 }
 
 #[cfg(test)]
@@ -47,7 +74,12 @@ mod tests {
     use crate::test::helper::create_test_db;
 
     use super::*;
-    use actix_web::{http::{header::ContentType, StatusCode}, test, test::TestRequest, App};
+    use actix_web::{
+        http::{header::ContentType, StatusCode},
+        test,
+        test::TestRequest,
+        App,
+    };
 
     #[actix_web::test]
     async fn test_register() {
@@ -55,7 +87,9 @@ mod tests {
         let app = test::init_service(App::new().app_data(Data::new(db)).service(register)).await;
         let req = TestRequest::post()
             .uri("/register")
-            .set_payload(r#"{"name": "arian", "password": "idk", "phone_number": "09223768300"}"#)
+            .set_payload(
+                r#"{"name": "arian", "password": "idkkkkl", "phone_number": "09223768300"}"#,
+            )
             .insert_header(ContentType::json())
             .to_request();
 
@@ -70,7 +104,7 @@ mod tests {
 
         let req = TestRequest::post()
             .uri("/register")
-            .set_payload(r#"{"name": "arian", "password": "idk", "phone_number": "09224769300"}"#)
+            .set_payload(r#"{"name": "arian", "password": "idkkk", "phone_number": "09224769300"}"#)
             .insert_header(ContentType::json())
             .to_request();
         let resp = test::call_service(&app, req).await;
@@ -78,7 +112,7 @@ mod tests {
 
         let req = TestRequest::post()
             .uri("/register")
-            .set_payload(r#"{"name": "pouya", "password": "okk", "phone_number": "09224769300"}"#)
+            .set_payload(r#"{"name": "pouya", "password": "okkok", "phone_number": "09224769300"}"#)
             .insert_header(ContentType::json())
             .to_request();
         let resp = test::call_service(&app, req).await;
@@ -91,10 +125,26 @@ mod tests {
         let app = test::init_service(App::new().app_data(Data::new(db)).service(register)).await;
         let req = TestRequest::post()
             .uri("/register")
-            .set_payload(r#"{"name": "arian", "password": "idk", "phone_number": "0922"}"#)
+            .set_payload(r#"{"name": "arian", "password": "idkkk", "phone_number": "0922"}"#)
             .insert_header(ContentType::json())
             .to_request();
         let resp = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[actix_web::test]
+    async fn test_sha256_hash() {
+        assert_eq!(
+            sha256_hash("salam"),
+            "0582bd2c13fff71d7f40ef5586e3f4da05a3a61fe5ba9f0b4d06e99905ab83ea"
+        );
+        assert_eq!(
+            sha256_hash("hello are you ok #!?"),
+            "d5ef5c1a3a959f846ae09ebe1472a51a7ae784a3f726457d8939e833f8f1d7ce"
+        );
+        assert_eq!(
+            sha256_hash("12345Aa@&$hello%^"),
+            "60fcd6b50b3d0d0bbf8d13ed5ff7e4b1844a1239fec1a94b0fe189222670e832"
+        );
     }
 }
--- src/test/helper.rs
+++ src/test/helper.rs
@@ -1,14 +1,19 @@
 use crate::db::{establish_connection, setup, DbPool};
+use anyhow::Result;
 use dotenv::dotenv;
 
 pub async fn create_test_db() -> DbPool {
     dotenv().ok();
     let pool = establish_connection().await.unwrap();
-    sqlx::query!("DROP TABLE IF EXISTS users")
-        .execute(&pool)
-        .await
-        .unwrap();
-
+    reset_db(&pool).await.unwrap();
     setup(&pool).await.unwrap();
     pool
+}
+
+async fn reset_db(pool: &DbPool) -> Result<()> {
+    sqlx::query!("DROP TABLE IF EXISTS users")
+        .execute(pool)
+        .await?;
+
+    Ok(())
 }
